71a064ad4af42c2777486fc7978e2d59b4dc176a
James Dindi <dindijjames@gmail.com>
2018-07-06 14:35:18 +0300

    [chore #158837719] Configure the settings file (#5)
    ### What does this PR do?
    Adds environment variables to the settings file.
    
    ### Description of Task to be completed?
    Add environment variables to settings file so as to enhance the security of the application's private data
    
    ### How should this be manually tested?
    When one accesses the settings.py file, no sensitive data should be seen.
    
    ## PT Story
    * 158837719

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
 from wger.settings_global import *
 
 # Use 'DEBUG = True' to get more details for server errors
@@ -15,31 +15,31 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': './database.sqlite',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = ')8-woct4l-fk2(gs&z@uyizjnaw_yg#5rm8$m_xx8szpb)lyxp'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Your reCaptcha keys
-RECAPTCHA_PUBLIC_KEY = ''
-RECAPTCHA_PRIVATE_KEY = ''
+RECAPTCHA_PUBLIC_KEY = os.getenv('RECAPTCHA_PUBLIC_KEY')
+RECAPTCHA_PRIVATE_KEY = os.getenv('RECAPTCHA_PRIVATE_KEY')
 NOCAPTCHA = True
 
 # The site's URL (e.g. http://www.my-local-gym.com or http://localhost:8000)
 # This is needed for uploaded files and images (exercise images, etc.) to be
 # properly served.
-SITE_URL = 'http://localhost:8000'
+SITE_URL = os.getenv('SITE_URL')
 
 # Path to uploaded files
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-MEDIA_ROOT = '/Users/emery/.local/share/wger/media'
+MEDIA_ROOT = os.getenv('MEDIA_ROOT')
 MEDIA_URL = '/media/'
 
 # Allow all hosts to access the application. Change if used in production.
